26b4c1323fefdcc73eae7af81b6b046b3dd9fa1c
Akiinro <aktakinro@gmail.com>
2021-09-19 20:13:17 -0700

    initial commit

---
+++ Assignment3.py
@@ -0,0 +1,37 @@
+le_List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+
+le_List.pop(5)
+le_List.pop(4)
+le_List.pop(0)
+print(le_List)
+
+
+
+
+
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+your_favorite_color = input('what is your favorite color: ')
+color_list.append(your_favorite_color)
+print(color_list)
+
+
+
+
+my_list = ['this', True, 'student', 45, 66.43]
+
+clone_list = my_list.copy()
+
+print(clone_list)
+print(my_list)
+
+
+
+list1 = [10,20,[300,400,[5000,6000],500],30,40]
+add_number = 7000
+
+list1[2].append(add_number)
+print(list1)
+
+
+
+ 
