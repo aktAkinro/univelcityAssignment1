bfc2e74e115aaa5ebbcc19ebb9e709bda43014f4
Akiinro <aktakinro@gmail.com>
2021-09-19 20:39:02 -0700

    list assignment

--- Assignment.py
+++ Assignment.py
@@ -1,18 +1,53 @@
+le_List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 
-# Given that g(x) = 2x**3 - 4x. Solve for g(7)
-# soln.   let x be equal to 7 then replace in equation
-
-x = 7
-gx = (2*(x**3))-4*x
-print(gx)
+le_List.pop(5)
+le_List.pop(4)
+le_List.pop(0)
+print(le_List)
 
 
-# write a program that converts 200 naira to dollars. asume 30 dollars is 1 naira.
-# soln.   if 30 dollars is 1 naira then 200 naira would be 30 dollars multiplied by 200 naira
 
-value_of_one_naira = 30
-naira_value_to_be_converted = 200
 
-currency_converter = value_of_one_naira*naira_value_to_be_converted
-print(currency_converter)
+
+color_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+your_favorite_color = input('what is your favorite color: ')
+color_list.append(your_favorite_color)
+print(color_list)
+
+
+
+
+my_list = ['this', True, 'student', 45, 66.43]
+
+clone_list = my_list.copy()
+
+print(clone_list)
+print(my_list)
+
+
+
+list1 = [10,20,[300,400,[5000,6000],500],30,40]
+add_number = 7000
+list1[2].append(add_number)
+print(list1)
+
+
+
+
+# # Given that g(x) = 2x**3 - 4x. Solve for g(7)
+# # soln.   let x be equal to 7 then replace in equation
+
+# x = 7
+# gx = (2*(x**3))-4*x
+# print(gx)
+
+
+# # write a program that converts 200 naira to dollars. asume 30 dollars is 1 naira.
+# # soln.   if 30 dollars is 1 naira then 200 naira would be 30 dollars multiplied by 200 naira
+
+# value_of_one_naira = 30
+# naira_value_to_be_converted = 200
+
+# currency_converter = value_of_one_naira*naira_value_to_be_converted
+# print(currency_converter)
 
