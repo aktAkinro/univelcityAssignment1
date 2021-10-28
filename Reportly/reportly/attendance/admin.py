12db8fa59f631671d623a592cf36d7b991bbc723
Akiinro <aktakinro@gmail.com>
2021-10-28 05:30:25 -0700

    reportly

--- Reportly/reportly/attendance/admin.py
+++ Reportly/reportly/attendance/admin.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Student
 
 # Register your models here.
 # admin.site.register(Book) # this is one way to register
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ["title", "no_of_pages", "date"]
+    list_display = ["title", "id", "no_of_pages", "date"]
     list_editable = ["no_of_pages"]
     list_filter = ["date"]
     list_per_page = 2
     search_fieldss = ["title", "author","body"]
+
+@admin.register(Student)
+class studentAdmin(admin.ModelAdmin):
+    display = ["First_name","id", "Last_name"]
+    editable = ["First_name"]
+    filter = ["First_name"]
+    
+    
+
+
+
+    
+
+# @admin.register(student)
+# class studentAdmin(admin.ModelAdmin):
+#     list_display = ["First_name","Last_name"]
+#     list_editable = ["First_name","Last_name"]
 
 
 
--- Reportly/reportly/attendance/models.py
+++ Reportly/reportly/attendance/models.py
@@ -6,10 +6,19 @@ class Book(models.Model):
     no_of_pages = models.IntegerField(default=10)
     author = models.CharField(max_length=300)
     body = models.TextField()
-    date = models.DateTimeField()
-    isbn = models.CharField(max_length=42)
+    date = models.DateTimeField(auto_now_add=True)
+    isbn = models.CharField(max_length=42, null=True, blank=True )
+    # isb = models.CharField(max_length=42, null=True, blank=True)
     
     def __str__(self):
         return self.title
 
+class Student(models.Model):
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    age = models.IntegerField(default=10)
+    course = models.CharField(max_length=300)
+    bio = models.TextField()
 
+    def __str__(self):
+        return self.first_name
